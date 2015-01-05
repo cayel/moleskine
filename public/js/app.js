031ec4203f0dd6f9f81af527cca06f651838ab9a
Christophe Ayel <christophe.ayel@gmail.com>
2015-01-05 21:19:52 +0100

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,2 @@
+// public/js/app.js
+angular.module('moleskineApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'BdCtrl', 'BdService']);
---
+++ public/js/controllers/BdCtrl.js
@@ -0,0 +1,6 @@
+// public/js/controllers/BdCtrl.js
+angular.module('BdCtrl', []).controller('BdController', function($scope) {
+
+  $scope.tagline = 'Des BDs, rien que des BDs ...';
+
+});
