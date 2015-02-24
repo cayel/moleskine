07840cd2dcdfab94a30d3343ca7c17114e0d5fce
Christophe Ayel <christophe.ayel@gmail.com>
2015-02-24 20:20:53 +0100

    ajout de la liste des élements avec le nombre en page accueil

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,25 @@
 // public/js/controllers/MainCtrl.js
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', function($scope, $http) {
 
   $scope.tagline = 'Ce que je lis et je regarde ...';
+	
+	// Liste des BDs
+	$http.get('/api/bds')
+	.success(function(data) {
+		$scope.bds = data;
+		// console.log(data);
+	})
+	.error(function(data) {
+		// console.log('Error: ' + data);
+	});
 
+	// Liste des Concerts
+	$http.get('/api/concerts')
+	.success(function(data) {
+		$scope.concerts = data;
+		// console.log(data);
+	})
+	.error(function(data) {
+		// console.log('Error: ' + data);
+	});
 });
